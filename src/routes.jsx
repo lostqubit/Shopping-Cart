5ebd519ea4d6fe8ad9547e009f84b69aa4469617
lostqubit <sg_suryaprakash@yahoo.com>
2024-06-28 21:36:22 +0530

    add route

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,9 +1,21 @@
 import App from "./components/App.jsx";
+import Home from "./components/Home.jsx";
+import Shop from "./components/Shop.jsx";
 
 const routes = [
 	{
 		path: "/",
 		element: <App />,
+		children: [
+			{
+				index: true,
+				element: <Home />,
+			},
+			{
+				path: "/shop",
+				element: <Shop />,
+			},
+		],
 	},
 ];
 
