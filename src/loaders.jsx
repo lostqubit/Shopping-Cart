546603bfadf45ad0c3389c7a6e664cc27d0872f6
lostqubit <sg_suryaprakash@yahoo.com>
2024-07-02 17:59:56 +0530

    add product loader

--- src/loaders.jsx
+++ src/loaders.jsx
@@ -21,3 +21,10 @@ export default async function productsLoader({ params }) {
 		return { data };
 	}
 }
+
+export async function productLoader({ params }) {
+	const response = await fetch(`https://fakestoreapi.com/products/${params.id}`);
+	const data = await response.json();
+
+	return { data };
+}
